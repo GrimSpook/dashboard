8acf646696c1d73a99e878a9799f3be7541749ac
Grimspook <46845957+GrimSpook@users.noreply.github.com>
2026-04-08 23:12:41 +0200

    fix: json data file

--- data/json.go
+++ data/json.go
@@ -3,19 +3,49 @@ package data
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
+func getDataDir() (string, string, error) {
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", "", err
+	}
+
+	dataDir := filepath.Join(home, ".config", "dashboard", "tracker-data")
+
+	file := filepath.Join(dataDir, "tracker-data.json")
+
+	return file, dataDir, nil
+}
+
 func SaveCompanies(companies []Company) error {
+	file, dir, err := getDataDir()
+	if err != nil {
+		return err
+	}
+
 	data, err := json.MarshalIndent(companies, "", "  ")
 	if err != nil {
 		return err
 	}
 
-	return os.WriteFile("tracker-data.json", data, 0644)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	return os.WriteFile(file, data, 0644)
 }
 
 func LoadCompanies() ([]Company, error) {
-	data, err := os.ReadFile("tracker-data.json")
+
+	file, _, err := getDataDir()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(file)
 	if err != nil {
 		return nil, err
 	}
